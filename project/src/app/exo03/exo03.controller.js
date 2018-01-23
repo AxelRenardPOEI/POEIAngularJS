b8510ec434503fc3fc8cde245c32d7aa0f2127f5
N4naki <axel.renard@epsi.fr>
2018-01-23 17:46:54 +0100

    Etape6

--- project/src/app/exo03/exo03.controller.js
+++ project/src/app/exo03/exo03.controller.js
@@ -5,11 +5,34 @@
     var vm = this;
 
 
+
+
+
 vm.films= [
 	{name: 'Fast and Furious', img: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/AkSMd8AXSpA1L1SbBPc2AfVJfmE.jpg', year: 2001},
 	{name: '2 Fast 2 Furious', img: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/u1Snfl40rXmAJNiMyZUczUyFVuf.jpg', year: 2003},
 	{name: 'Fast & Furious: Tokyo Drift', img: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/4ceW7kWPAc2uFG5WzYDbP8Od3gw.jpg', year: 2006}
-];  
+];
+
+    vm.SaveFilm = SaveFilm; 
+
+    function SaveFilm(nom) {
+      for (var i = vm.films.length - 1; i >= 0; i--) {
+        
+        if(vm.films[i].name === nom)
+        {
+          var filmSave = vm.films[i];
+          console.log(filmSave.name);
+        }
+      }
+      
+    }
+
+vm.alert = alert;
+
+function alert(text) {
+	console.log(text);
+}
 
 }
 
@@ -18,4 +41,4 @@ Exo03Ctrl.$inject = ['$location'];
   angular.module('daproject')
     .controller('Exo03Ctrl', Exo03Ctrl);
 
-})();
+})(); 
